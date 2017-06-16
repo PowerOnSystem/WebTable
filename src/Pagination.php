ceb7342a6dc05840a71832db67e6352f691d6bf8
Lucas Sosa <sosalucas87@gmail.com>
2017-06-16 13:26:53 -0300

    Initial Commit

---
+++ src/Pagination.php
@@ -0,0 +1,185 @@
+<?php
+
+/*
+ * Copyright (C) PowerOn Sistemas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PowerOn\Utility;
+
+/**
+ * Inflector
+ * @example
+ * <code>
+ *      $results = [];
+ *      $pagination = new Pagination();
+ *      $content = array_slice($results, $pagination->getStartResults(), $pagination->getEndResults());
+ * </code>
+ * @author Lucas Sosa
+ * @version 0.2
+ */
+class Pagination {
+    /**
+     * Página actual
+     * @var integer
+     */
+    private $_current_page;
+    /**
+     * Resultados a mostrar por página
+     * @var integer
+     */
+    private $_results_per_page;
+    /**
+     * Total de resultados
+     * @var integer
+     */
+    private $_max_results;
+
+    /**
+     * Crea una paginación de resultados
+     * @param integer $current_page La página solicitada por GET
+     * @param integer $max_results Máximos resultados de la tabla
+     * @param integer $results_per_page Resultados a mostrar por página
+     */
+    public function __construct($current_page, $max_results, $results_per_page) {
+        $this->_current_page = $current_page;
+        $this->_results_per_page = $results_per_page;
+        $this->_max_results = $max_results;
+    }
+    
+    /**
+     * Devuelve el número de páginas totales de la paginación
+     * @return integer
+     */
+    public function getNumberOfPages() {
+        return intval(ceil($this->_max_results / $this->_results_per_page));
+    }
+    
+    /**
+     * Devuelve el número de página actual cargada
+     * @return integer
+     */
+    public function getCurrentPage() {
+        $number_of_pages = $this->getNumberOfPages();
+        return $this->_current_page <= 0 ? 1 : (intval($this->_current_page < $number_of_pages ? $this->_current_page : $number_of_pages));
+    }
+    
+    /**
+     * Devuelve el número de resultados mostrados en pantalla
+     * @retur integer
+     */
+    public function getLoadedResults() {
+        return $this->getNumberOfPages() == $this->getCurrentPage() ? ($this->_max_results % $this->_results_per_page) : 
+            $this->_results_per_page;
+    }
+    
+    /**
+     * Devuelve la página siguiente o FALSE si llega al límite
+     * @return integer|boolean
+     */
+    public function getNextPage() {
+        $current_page = $this->getCurrentPage();
+        return $current_page < $this->getNumberOfPages() ? $current_page + 1 : FALSE;
+    }
+    
+    /**
+     * Devuelve la página anterior o FALSE si se encuentra en la primera
+     * @return integer|boolean
+     */
+    public function getPreviousPage() {
+        $current_page = $this->getCurrentPage();
+        return $current_page > 1 ? $current_page - 1 : FALSE;
+    }
+    
+    /**
+     * Devuelve las próximas 10 páginas o FALSE en caso de que sea el límite
+     * @return integer|boolean
+     */
+    public function getNextTenPages() {
+        $number_of_pages = $this->getNumberOfPages();
+        $current_page = $this->getCurrentPage();
+        return $current_page < $number_of_pages - 10 ? $current_page + 10 : FALSE;
+    }
+    
+    /**
+     * Devuelve las 10 páginas anteriores o FALSE en caso de que existan
+     * @return integer|boolean
+     */
+    public function getPreviousTenPages() {
+        $current_page = $this->getCurrentPage();
+        return $current_page >= 20 ? $current_page - 10 : FALSE;
+    }
+    
+    /**
+     * Verifica si debe mostrarse la primer página
+     * @return boolean
+     */
+    public function showFirstPage() {
+        return $this->getCurrentPage() >= 10;
+    } 
+    
+    /**
+     * Verifica si debe mostrarse la última página
+     * @return boolean
+     */
+    public function showLastPage() {
+        $number_of_pages = $this->getNumberOfPages();
+        $current_page = $this->getCurrentPage();
+        $start_pagination = intval($current_page / 10) * 10;
+        $end_pagination = $start_pagination ? $start_pagination + 10 : 10;
+        
+        return $number_of_pages > 10 && $end_pagination < $number_of_pages;
+    } 
+    
+    /**
+     * Devuelve el número donde comienza la paginación de resultados
+     * @return integer
+     */
+    public function getStartPagination() {
+        $current_page = $this->getCurrentPage();
+        $start_pagination = intval($current_page / 10) * 10;
+        return $start_pagination ? $start_pagination : 1;
+    }
+    
+    /**
+     * Devuelve el número donde finaliza la paginación de resultados
+     * @return integer
+     */
+    public function getEndPagination() {
+        $number_of_pages = $this->getNumberOfPages();
+        $current_page = $this->getCurrentPage();
+        $start_pagination = intval($current_page / 10) * 10;
+        $end_pagination = $start_pagination ? $start_pagination + 10 : 10;
+        
+        return $end_pagination < $number_of_pages ? $end_pagination : $number_of_pages;
+    }
+    
+    /**
+     * Devuelve el número donde inician los resultados para una consulta MySql
+     * @return type
+     */
+    public function getStartResults() {
+        $current_page = $this->getCurrentPage();
+        return $current_page > 1 ? ($current_page - 1) * $this->_results_per_page : 0;
+    }
+    
+    /**
+     * Devuelve el número donde finalizan los resultados para una consulta MySql
+     * @return integer
+     */
+    public function getEndResults() {
+        return $this->_results_per_page;
+    }
+}
---
+++ src/Table.php
@@ -0,0 +1,417 @@
+<?php
+
+/*
+ * Copyright (C) PowerOn Sistemas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace PowerOn\Utility;
+use PowerOn\Exceptions\DevException;
+use function \PowerOn\Application\array_trim;
+
+/**
+ * Table
+ * @author Lucas Sosa
+ * @version 0.1
+ */
+class Table {
+    /**
+     * Contenido
+     * @var array
+     */
+    private $_body = [];
+    /**
+     * Encabezado
+     * @var array
+     */
+    private $_header = [];
+    /**
+     * Pie
+     * @var array
+     */
+    private $_footer = [];
+    /**
+     * Parámetros de configuracion de fila
+     * @var array
+     */
+    private $_rows_params = [];
+    /**
+     * Puntero interno de la fila para el método cell
+     * @var integer
+     */
+    private $_pointer = 0;
+    /**
+     * Configuración de la tabla
+     * @var array
+     */
+    private $_config = [];
+    /**
+     * Especifica si se puede ordenar los resultados
+     * @var boolean 
+     */
+    public $sortable = FALSE;
+    /**
+     * Campo indice de orden
+     * @var string
+     */
+    public $sort_by = NULL;
+    /**
+     * Modo de orden ascendente o descendente [asc|desc]
+     * @var string
+     */
+    public $sort_mode = NULL;
+    
+    /**
+     * Inicia una nueva tabla
+     * @example
+     * <code>
+     *      $params = [
+     *          'title' => 'Nueva Tabla',
+     *          'width' => '100%',
+     *          'class' => 'mytable',
+     *          'id' => 'nwtbl',
+     *          'sortable' => TRUE,
+     *          'sort_by' => 'id',
+     *          'sort_mode' => 'asc'
+     *      ]
+     * </code>
+     * @param mix $params Los parámetros en Array o el título de la tabla
+     * @throws DevException
+     */
+    public function __construct( $params = NULL ) {
+        if ( is_string($params) ) {
+            $params = ['title' => $params];
+        } else if ( $params && !is_array($params) ) {
+            throw new DevException('Formato de configuraci&oacute;n de tabla incorrecto', ['params' => $params]);
+        } else if ( !$params ) {
+            $params = [];
+        }
+        
+        $this->sortable = array_trim($params, 'sortable');
+        $this->sort_by = array_trim($params, 'sort_by');
+        $this->sort_mode = array_trim($params, 'sort_mode');
+        
+        $this->_config = $params + [
+            'title' => NULL,
+            'width' => 'auto',
+            'class' => NULL,
+            'id' => NULL,
+        ];
+    }
+    
+    /**
+     * Inicia el encabezado de la tabla
+     * @example 
+     * <code>
+     *      $table = new Table();
+     *      $table->header([
+     *          'id' => ['title' => 'Codigo', 'width' => '15%'],
+     *          'name' => 'Nombre'
+     *          'client' => ['title' => 'Nro. de Cliente', 'sort_name' => 'internal_code']
+     *      ]);
+     * </code>
+     * @param array $header
+     * @return \PowerOn\Utility\Table
+     */
+    public function header(array $header) {
+        foreach ($header as $name => $head) {
+            $this->head($name, $head);
+        }
+        return $this;
+    }
+    
+    /**
+     * Agrega un elemento al encabezado
+     * @example 
+     * <code>
+     *      $table = new Table();
+     *      $table
+     *          ->head('id', 'Codigo')
+     *          ->head('name', 'Nombre')
+     *          ->head('client', ['title' => 'Nro. de Cliente', 'sort_name' => 'internal_code'])
+     *          ...
+     * </code>
+     * @param string $name El nombre de la columna
+     * @param string|array $head Los parámetros de la columna o el título
+     * @throws DevException
+     * @return \PowerOn\Utility\Table
+     */
+    public function head($name, $head) {
+        if ( is_string($head) ) {
+            $head = ['title' => $head];
+        } else if ( !is_array($head) ) {
+            throw new DevException('Formato de configuración de cabezera de tabla incorrecto', ['name' => $name, 'error' => $head]);
+        }
+
+        $this->_header[$name] = $head + [
+            'title' => NULL,
+            'width' => 'auto',
+            'class' => NULL,
+            'sort_name' => NULL
+        ];
+        return $this;
+    }
+    
+    /**
+     * Inicia el pie de la tabla
+     * @example 
+     * <code>
+     *      $table = new Table();
+     *      $table->header(...);
+     *      $table->footer([
+     *          'subtotal' => '$2500',
+     *          'total' => ['title' => '$13599', class => 'green']
+     *      ]);
+     * </code>
+     * @param array $footer
+     * @return \PowerOn\Utility\Table
+     */
+    public function footer( array $footer) {
+        foreach ($footer as $name => $foot) {
+            $this->foot($name, $foot);
+        }
+        return $this;
+    }
+    
+    /**
+     * Agrega un elemento al pie
+     * @example 
+     * <code>
+     *      $table = new Table();
+     *      $table->header(...);
+     *      $table
+     *          ->foot('subtotal', '$2500')
+     *          ->foot('total', ['title' => '$13599', class => 'green'])
+     *          ...
+     * </code>
+     * @param string $name El nombre de la columna
+     * @param string|array $foot Los parámetros de la columna o el título
+     * @throws DevException
+     * @return \PowerOn\Utility\Table
+     */
+    public function foot($name, $foot) {
+        if ( is_string($foot) ) {
+            $foot = ['title' => $foot];
+        } else if ( $foot === NULL ) {
+            $foot = ['title' => NULL];
+        } else if ( !is_array($foot) ) {
+            throw new DevException('Formato de configuración de pie de tabla incorrecto', ['name' => $name, 'error' => $foot]);
+        }
+
+        $this->_footer[$name] = $foot + [
+            'title' => '',
+            'width' => 'auto',
+            'class' => NULL,
+        ];
+        return $this;
+    }
+    
+    /**
+     * Establece el contenido de una tabla
+     * @example 
+     * <code>
+     *      $table = new Table();
+     *      $table->header([
+     *          'id' => 'Codigo',
+     *          'name' => 'Nombre'
+     *          ...
+     *      ]);
+     *      $table->body([
+     *          ['id' => 0030, 'name' => 'Carlos', ...],
+     *          [
+     *              '_row_param' => [...], //Parámetros opcionales de la fila
+     *              'id' => 0031, 
+     *              'name' => 'Sergio', 
+     *              'client' => ['title' => '0021518', 'link' => ['controller' => 'clientes', 'action' => 'view', '21518']
+     *              ...
+     *          ],
+     *          ...
+     *      ]);
+     * </code>
+     * @param array $body
+     * @throws DevException
+     */
+    public function body(array $body) {
+        foreach ($body as $row_id => $row) {
+            $row_param = array_trim($row, '_row_param');
+            $this->row($row, $row_param ? $row_param : [], $row_id);
+        }
+    }
+    
+    /**
+     * Agrega una fila completa al contenido de la tabla
+     * @example
+     * <code>
+     *      $table = new Table();
+     *      $table->header([
+     *          'id' => 'Codigo',
+     *          'name' => 'Nombre'
+     *          ...
+     *      ]);
+     *      $table->row([
+     *          'id' => 0030,
+     *          'name' => 'Carlos',
+     *          'client' => ['title' => '0021518', 'link' => ['controller' => 'clientes', 'action' => 'view', '21518'],
+     *          ...
+     *      ]);
+     * </code>
+     * @param array $row
+     * @param array $row_params [Opcional] Parámetros de configuración de la fila 
+     * @example
+     * <code>
+     *      $row_params = [
+     *          'class' => 'warning'
+     *          'id' => 'row75'
+     *          ...
+     *      ]
+     * </code>
+     * @param integer $row_id [Opcional] El puntero de la columna, por defecto es autoincremental
+     * @return \PowerOn\Utility\Table
+     */
+    public function row(array $row, array $row_params = [], $row_id = NULL) {
+        $this->_rows_params[$row_id] = $row_params;
+        foreach ($row as $column_name => $data) {
+            $this->cell($column_name, $data, $row_id !== NULL ? $row_id : count($this->_body));
+        }
+        return $this;
+    }
+    
+    /**
+     * Establce los parámetros de una fila específica
+     * @param array $row_params
+     * @param integer $row_id [Opcional] Si no se especifica utiliza el puntero interno.
+     * @return \PowerOn\Utility\Table
+     */
+    public function params(array $row_params, $row_id = NULL) {
+        $this->_rows_params[$row_id ? $row_id : $this->_pointer] = $row_params;
+        return $this;
+    }
+    
+    /**
+     * Agrega una celda a una fila del contenido de la tabla
+     * @example
+     * <code>
+     *      $table = new Table();
+     *      $table->header([
+     *          'id' => 'Codigo',
+     *          'name' => 'Nombre'
+     *          ...
+     *      ]);
+     *      $table
+     *          ->cell('id', 0030)
+     *          ->cell('name', 'Carlos')
+     *          ->cell('client', ['title' => '0021518', 'link' => ['controller' => 'clientes', 'action' => 'view', '21518']]
+     *          ...
+     * </code>
+     * @param string $column_name El nombre de la columna
+     * @param string|array $data Array con parámetros de la celda o el título
+     * @param integer $row_id [Opcional] El identificador de la fila, si no se especifica se utiliza
+     * el puntero interno de la tabla, si desea agregar una nueva fila utilize la función <b>Table::next()</b>
+     * @throws DevException
+     * @return \PowerOn\Utility\Table
+     */
+    public function cell($column_name, $data, $row_id = NULL) {
+        if ( is_string($data) ) {
+            $data = ['title' => $data];
+        } else if ($data === NULL) {
+            $data = ['title' => NULL];
+        } else if ( !is_array($data) ) {
+            throw new DevException('Formato de configuración del contenido de la tabla incorrecto', 
+                    ['row_id' => $row_id, 'error' => $data]);
+        }
+        
+        $this->_body[$row_id][$column_name] = $data + [
+            'title' => '',
+            'class' => NULL,
+            'link' => NULL
+        ];
+        
+        return $this;
+    }
+    
+    /**
+     * Modifica el puntero interno de la fila de la tabla
+     * @example
+     * <code>
+     *      $table = new Table();
+     *      $table->header(...);
+     *      $table
+     *          ->cell(...)
+     *          ->cell(...)
+     *          ...
+     * 
+     *          ->next(..)
+     * 
+     *          ->cell(...)
+     *          ->cell(...);
+     * </code>
+     * @return \PowerOn\Utility\Table
+     */
+    public function next() {
+        ++ $this->_pointer;
+        return $this;
+    }
+    
+    /**
+     * Resetea el contenido de la tabla
+     */
+    public function reset() {
+        $this->_pointer = 0;
+        $this->_header = [];
+        $this->_body = [];
+        $this->_config = [];
+        $this->_rows_params = [];
+    }
+    
+    /**
+     * Devuelve la configuración de la tabla
+     * @return array
+     */
+    public function getConfig() {
+        return $this->_config;
+    }
+    
+    /**
+     * Devuelve la cabecera de la tabla
+     * @return array
+     */
+    public function getHeader() {
+        return $this->_header ? $this->_header : [];
+    }
+    
+    /**
+     * Devuelve el contenido de la tabla
+     * @return array
+     */
+    public function getBody() {
+        return $this->_body ? $this->_body : [];
+    }
+    
+    /**
+     * Devuelve el contenido del pie de la tabla
+     * @return array
+     */
+    public function getFooter() {
+        return $this->_footer ? $this->_footer : [];
+    }
+    
+    /**
+     * Devuelve los parámetros de configuración de filas
+     * @return array
+     */
+    public function getRowsParams() {
+        return $this->_rows_params ? $this->_rows_params : [];
+    }
+}
